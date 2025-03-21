233ba1999fb8f3ce7e5ae8ab5ce62727862d5932
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2025-03-21 14:29:24 +0000

    I added js, html, and css

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ms Raffin
+// Created on: Mar 2022
 // This file contains the JS functions for index.html
+
+function doMathClicked () {
+  // this function does basic math
+    document.getElementById('add-math').innerHTML = 'Perimeter of the rectangle = ' + (5 + 5 + 3 + 3) + ' cm';
+  document.getElementById('multiply-math').innerHTML = 'Area of the rectangle = ' + (5 * 3) + ' cm²';
+
+
+}
